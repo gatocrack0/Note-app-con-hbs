9f004609dbf9f9d710cbcf1d646046272125edd7
Vicente Barra <vicente.barra.94@gmail.com>
2020-12-12 22:43:32 +0000

    make it better

--- src/database.js
+++ src/database.js
@@ -1,11 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/notes-db-app',{
+/* mongoose.connect('mongodb://localhost/notes-db-app',{
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
     .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
+    .catch(err => console.error(err)); */
 
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://gatocrack0:gatoblack2005@freecluster.qyhrk.mongodb.net/FreeCluster?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
