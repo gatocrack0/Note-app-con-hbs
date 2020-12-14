5de072e7609cb22d6f01e8b3a5a20e0d533014eb
Vicente Barra <vicente.barra.94@gmail.com>
2020-12-14 06:08:11 +0000

    upgrades

--- src/database.js
+++ src/database.js
@@ -11,13 +11,3 @@ mongoose.connect(MONGODB_URI,{
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err)); 
 
-/* const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://gatocrack0:gatoblack2005@freecluster.qyhrk.mongodb.net/FreeCluster?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
- */
